f57764f61ef08f3020190bd2d47a3963ad5ce6b1
Wallace wambulwa <wallacewambulwa@gmail.com>
2023-10-05 23:39:33 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <time.h>
+
+#include <stdlib.h>
+
+/**
+ * main - a program that prints numbers
+ * Return: Always a (success)
+ */
+
+int main(void)
+
+{
+int d;
+for (d = 'a'; d > 'a'; d--)
+putchar('d');
+putchar('\n');
+
+return (0);
+
+}
