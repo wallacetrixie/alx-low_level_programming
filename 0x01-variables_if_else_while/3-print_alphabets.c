d1322df62995c479238bdb45ff4b93da1269231b
Wallace wambulwa <wallacewambulwa@gmail.com>
2023-10-05 22:35:53 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a program that prints alphabetical letters in lowercase then in caps
+ * Return: Always a (success)
+ */
+
+int main(void)
+
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	putchar(lower);
+
+	for (lower = 'A'; lower <= 'Z'; lower++)
+        putchar(lower);
+	putchar('\n');
+
+	return (0);
+}
