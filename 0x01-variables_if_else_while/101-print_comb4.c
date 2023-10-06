3fffefd1d8798775593f478a3a29d3d6fb7650fa
Wallace wambulwa <wallacewambulwa@gmail.com>
2023-10-06 00:26:29 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#include <time.h>
+
+#include <stdlib.h>
+
+/**
+ * main - a program that prints numbers
+ * Return: Always a (success)
+ *
+ */
+
+int main(void)
+
+{
+int d;
+int p;
+int q;
+for (d = '0'; d < '9'; d++)
+{
+for (p = d + 1; p <= '9'; p++)
+{
+for (q = p + 1; q <= '9'; q++)
+{
+if ((p != d) != q)
+{
+putchar(d);
+putchar(p);
+putchar(q);
+if (d == '7' && p == '8')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
