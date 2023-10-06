9c215a0a6be52fd8a87bb4939c9e223e61606ec6
Wallace wambulwa <wallacewambulwa@gmail.com>
2023-10-05 23:07:39 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-print_numbers.c -o 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints numbers
+ * Return: Always a (success)
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf("\n");
+return (0);
+}
