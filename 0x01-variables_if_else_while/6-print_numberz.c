571ef8fccf100e38290550ce678d43bc4452a95c
Wallace wambulwa <wallacewambulwa@gmail.com>
2023-10-05 23:27:33 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - a program that prints numbers
+ * Return: Always a (success)
+ *
+ */
+
+int main(void)
+{
+
+int d;
+for (d = '0'; d <= '9'; d++)
+putchar('d');
+putchar('\n');
+return (0);
+
+}
