30a300799b5197713e98eeb8432ddf77610d1aba
Wallace wambulwa <wallacewambulwa@gmail.com>
2023-10-06 02:39:12 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,35 @@
+
 #include <stdio.h>
 
-#include <time.h>
-
-#include <stdlib.h>
-
 /**
- * main - a program that prints numbers
- * Return: Always a (success)
- *
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
  */
-
 int main(void)
+{
+	int i, j;
 
-{
-int p;
-int q;
-for (p = 0; p < 98; p++)
-{
-for (q = p + 1; q <= 99; q++)
-{
-putchar((p / 10) + '0');
-putchar((p % 10) + '0');
-putchar(',');
-putchar((q / 10) + '0');
-putchar((q % 10) + '0');
-if (p == 98 && q == 99)
-continue;
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 
+		}
+	}
+	putchar('\n');
+	return (0);
 }
+
