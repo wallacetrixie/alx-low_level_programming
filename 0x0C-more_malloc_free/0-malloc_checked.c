f5b347045ddc6233ff5ab948551f432a2dab0eae
Wallace wambulwa <wallacewambulwa@gmail.com>
2023-11-02 02:16:09 -0700

    code updated successfully

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,45 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 /**
-* malloc_checked - cause normal process termination with a status value of 98
-*
-* @b: allocated memory
-*
-* Return: a pointer to the allocated memory
-*/
+ * malloc_checked - Allocates memory using malloc.
+ * @b: Number of bytes to allocate.
+ *
+ * Return: A pointer to the allocated memory.
+ * Exits with status 98 on malloc failure.
+ */
 void *malloc_checked(unsigned int b)
 {
-void *i;
-i = malloc(b);
-if (i == NULL)
-exit(98);
-return (i);
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98); /* Terminate with status 98 on malloc failure */
+
+	return (ptr); /* Add parentheses here */
 }
-i
+
+int main(void)
+{
+	char *c;
+	int *i;
+	float *f;
+	double *d;
+
+	c = malloc_checked(sizeof(char) * 1024);
+	printf("%p\n", (void *)c);
+	i = malloc_checked(sizeof(int) * 402);
+	printf("%p\n", (void *)i);
+	f = malloc_checked(sizeof(float) * 100000000);
+	printf("%p\n", (void *)f);
+	d = malloc_checked(INT_MAX);
+	printf("%p\n", (void *)d);
+
+	free(c);
+	free(i);
+	free(f);
+	free(d);
+
+	return (0);
+}
+
