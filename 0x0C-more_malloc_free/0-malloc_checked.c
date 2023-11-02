d01dc1f8c2d9b1c68bafbdabc55f16a2cf09f982
Wallace wambulwa <wallacewambulwa@gmail.com>
2023-11-02 02:31:42 -0700

    changed

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+#include "main.h"
 
 /**
  * malloc_checked - Allocates memory using malloc.
@@ -11,35 +9,11 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+	void *i;
 
-	if (ptr == NULL)
-		exit(98); /* Terminate with status 98 on malloc failure */
-
-	return (ptr); /* Add parentheses here */
-}
-
-int main(void)
-{
-	char *c;
-	int *i;
-	float *f;
-	double *d;
-
-	c = malloc_checked(sizeof(char) * 1024);
-	printf("%p\n", (void *)c);
-	i = malloc_checked(sizeof(int) * 402);
-	printf("%p\n", (void *)i);
-	f = malloc_checked(sizeof(float) * 100000000);
-	printf("%p\n", (void *)f);
-	d = malloc_checked(INT_MAX);
-	printf("%p\n", (void *)d);
-
-	free(c);
-	free(i);
-	free(f);
-	free(d);
-
-	return (0);
+	i = malloc(b);
+	if (i == NULL)
+		exit(98);
+	return (i);
 }
 
