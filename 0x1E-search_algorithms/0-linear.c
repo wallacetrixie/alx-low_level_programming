5e53a66efb5a43213f0299b5f916cd28688abd63
wallacewambulwa@gmail.com <wallacewambulwa@gmail.com>
2024-05-06 22:56:18 -0700

    search algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,30 +1,25 @@
 #include "search_algos.h"
-#include <stdio.h>
 
 /**
- * linear_search - Searches for a value in an array of integers
- *                 using the Linear search algorithm.
- * @array: A pointer to the first element of the array to search in.
- * @size: The number of elements in the array.
- * @value: The value to search for.
- *
- * Return: The first index where value is located,
- *         or -1 if the value is not present in the array or if array is NULL.
+ * linear_search - searches for a value in a sorted array of integers
+ * @array: array of integers
+ * @size: size of array
+ * @value: value to search for
+ * Return: index of value or -1 if not found
  */
+
+
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
+	size_t inc;
 
-    if (array == NULL)
-        return (-1);
-
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
-
-    return (-1);
+	if (array == NULL)
+		return (-1);
+	for (inc = 0; inc < size; inc++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", inc, array[inc]);
+		if (array[inc] == value)
+			return (inc);
+	}
+	return (-1);
 }
-
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,47 +5,55 @@
 #include <stdlib.h>
 #include <math.h>
 
-/* 0. Linear search */
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
+
 int linear_search(int *array, size_t size, int value);
-
-/* 1. Binary search */
 int binary_search(int *array, size_t size, int value);
-
-/* 2. Big O #0 */
-int binary_search(int *array, size_t size, int value);
-
-/* 3. Big O #1 */
-/* Time complexity (worst case) of a linear search in an array of size n is O(n) */
-
-/* 4. Big O #2 */
-/* Space complexity (worst case) of an iterative linear search algorithm in an array of size n is O(1) */
-
-/* 5. Big O #3 */
-/* Time complexity (worst case) of a binary search in an array of size n is O(log n) */
-
-/* 6. Big O #4 */
-/* Space complexity (worst case) of a binary search in an array of size n is O(1) */
-
-/* 7. Big O #5 */
-/* Space complexity of the allocate_map function is O(n * m) */
-
-/* 8. Big O #6 */
-/* Time complexity (average case) of a jump search in an array of size n, using step = sqrt(n), is O(sqrt(n)) */
-
-/* 9. Big O #7 */
-/* Time complexity (average case) of an interpolation search in an array of size n is O(log log n) */
-
-/* 10. Big O #8 */
-/* Time complexity (average case) of an exponential search in an array of size n is O(log n) */
-
-/* 11. Advanced binary search */
+int _binary_search(int *array, size_t left, size_t right, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
 int advanced_binary(int *array, size_t size, int value);
-
-/* 12. Jump search in a singly linked list */
 listint_t *jump_list(listint_t *list, size_t size, int value);
-
-/* 13. Linear search in a skip list */
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
-#endif /* SEARCH_ALGOS_H */
-
+#endif
